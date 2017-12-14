57f7b749ca1bd6ba23cc1e510b56140626d011a9
Denis Semenikhin <denis_semenikhin@epam.com>
2017-12-14 16:34:03 +0300

    done with split method

--- src/spacify.js
+++ src/spacify.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-extend-native */
 Object.defineProperty(String.prototype, 'spacify', {
     value() {
-        return [...this].join(' ');
+        return this.split('').join(' ');
     }
 });
